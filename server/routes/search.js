44c5437e83294b6ae33be24aca3c5d50dfea3d19
smirnov-vv <vsmirnov.pro@gmail.com>
2025-03-17 19:44:52 +0300

    feat: sanitize client search request

--- server/routes/search.js
+++ server/routes/search.js
@@ -1,7 +1,11 @@
-export default async function userRoutes(fastify) {
+const searchRoutes = async (fastify) => {
   fastify.get('/search', async (request, reply) => {
     const isLoggedIn = request.cookies?.token ? true : false;
     const search = request.query.content;
+    // Remove html tags
+    const withoutTags = search.replace(/<[^>]*>/g, '');
+    // Replace all symbols used in XSS, excluding single quote, with space
+    const sanitized = withoutTags.replace(/[<>"`&\/(){}=;:\\]/g, ' ');
     const toPhoneticHTML =
       '<div id="toPhoneticsSpinner", class="spinner-border text-info" role="status"><span class="visually-hidden">Loading...</span></div>';
     const cambridgeHTML =
@@ -9,10 +13,12 @@ export default async function userRoutes(fastify) {
     return reply.view('/index.pug', {
       title: 'Result for:',
       message: '',
-      search,
+      search: sanitized,
       toPhoneticHTML,
       cambridgeHTML,
       isLoggedIn,
     });
   });
-}
+};
+
+export default searchRoutes;
